acbd4e6c091a176ff4931d78f9d70c8cc2a9d1c6
kemm0 <juho.ktokoi@outlook.com>
2020-11-25 20:10:13 +0200

    Added heroku stuff

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const cors = require('cors')
 
+const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('content', function (req, res) { 
   if(req.method == 'POST'){
@@ -88,14 +90,13 @@ app.post('/api/persons/',(request, response) => {
     name: name,
     number: number
   }
-  console.log(person)
   persons = persons.concat(person)
 
   return response.json(person)
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
